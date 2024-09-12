502b1bb82b8ce870184f372b1a9e8caf4ee88394
ayokanmi Samuel <ayokanmi.samuel@abbaandking.com>
2024-09-12 18:06:53 +0100

    hamburger update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router} from '@angular/router';
 import { TestimonialComponent } from '../testimonial/testimonial.component';
 
@@ -10,37 +10,7 @@ import { TestimonialComponent } from '../testimonial/testimonial.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
- constructor (private router:Router){
-  var menu = document.getElementsByClassName('header__desktop');
-  const hamburger = document.getElementsByClassName('hamburger')
-  const header = document.getElementsByClassName('header__mobile')
-  const header__wrapper = document.getElementsByClassName('header__wrapper')
-  let hamburgerr: HTMLImageElement[];
-  
-  
-    const openmenu =() => {
-      menu[0].classList.toggle('active')
-      if(menu[0].className.includes('active')){
-          hamburgerr[0].src = '/assets/cancel__icon.svg'
-      }
-      else{
-          hamburgerr[0].src = '/assets/hamburger.svg'
-      }
-  
-    }
-  
-  window.addEventListener('scroll', (e)=> {
-      if(window.scrollY > 0){
-          header[0].classList.add('header__bar')
-          header__wrapper[0].classList.add('header__bar')
-      }
-      else{
-          header[0].classList.remove('header__bar')
-          header__wrapper[0].classList.remove('header__bar')
-  
-      }
-    
-  }
-  )
- }
+ constructor ( private router: Router){
+
+}
 }
